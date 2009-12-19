0f934132b939267cc23360aeb097dccbc92ee812
yappo <yappo@shibuya.pl>
2009-12-19 20:34:57 +0900

    initial commit

---
+++ lib/MyTest/Foo1.pm
@@ -0,0 +1,32 @@
+package MyTest::Foo1;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+MyTest::Foo1 -
+
+=head1 SYNOPSIS
+
+  use MyTest::Foo1;
+
+=head1 DESCRIPTION
+
+MyTest::Foo1 is
+
+=head1 AUTHOR
+
+Default Name E<lt>yappo <at> shibuya <döt> plE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
